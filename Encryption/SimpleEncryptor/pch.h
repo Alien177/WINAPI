1485b04f4d4570c36276905bc5311b5ca6671ce5
Alien177 <39193276+Alien177@users.noreply.github.com>
2024-06-24 10:37:13 -0600

    Create pch.h

---
+++ Encryption/SimpleEncryptor/pch.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <Windows.h>
+#include <string>
+
+#include "..\..\cryptopp890\rsa.h"
+#include "..\..\cryptopp890\files.h"
+#include "..\..\cryptopp890\osrng.h"
+#include "..\..\cryptopp890\chacha.h"
